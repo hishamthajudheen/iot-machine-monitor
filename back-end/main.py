5898a841a2787b09af05de67f7d79dc52198b72d
Hisham Thajudheen <hishamthajudheen@gmail.com>
2023-04-11 20:06:49 +0530

    Some database test files added

--- back-end/main.py
+++ back-end/main.py
@@ -1,9 +1,7 @@
-from fastapi import FastAPI
-from starlette.responses import RedirectResponse
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 app = FastAPI()
-github_client_id = 'fa0752a4281fd3074eb4'
-
-@app.get("/github-login")
-async def github_login():
-    return RedirectResponse(f'https://github.com/login/oauth/authorize?client_id={github_client_id}',status_code=302)
+@app.get("/test/")
+async def test():
+    return {"hello":"world"}
