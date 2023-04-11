df75d6f1c3565899ca31830460d38a5eb4aa016a
Hisham Thajudheen <hishamthajudheen@gmail.com>
2023-04-11 11:00:40 +0530

    Added a requirements.txt for easier installations

--- back-end/main.py
+++ back-end/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 
 app = FastAPI()
+github_client_id = 'fa0752a4281fd3074eb4'
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/github-login")
+async def github_login():
+    return RedirectResponse(f'https://github.com/login/oauth/authorize?client_id={github_client_id}',status_code=302)
