ad25152023be74c8b92759c640db8d5d1d893e34
Nandana Anil <anilnandana51@gmail.com>
2023-06-16 00:11:29 +0530

    front-end react home

--- front-end react/mini-project/src/components/home.jsx
+++ front-end react/mini-project/src/components/home.jsx
@@ -4,11 +4,13 @@ import bgv1 from "../assets/bgv1.mp4";
 const Home = () =>{
     return(
         <div>
+            <div name="Home">
             <video autoPlay loop muted className="h-screen w-full object-cover">
                 <source src={bgv1} type="video/mp4" />
             </video>
+            </div>
             
-            </div> 
+        </div> 
     );
 }
 
