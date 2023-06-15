fce0e03da40b69ed89b6f771a8be51c191ae6b6a
Nandana Anil <anilnandana51@gmail.com>
2023-06-15 21:26:13 +0530

    front-end react home page

---
+++ front-end react/mini-project/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import SignupPage from './pages/Signup';
+import LoginPage from './pages/Login';
+import Navbar from "./components/Navbar";
+import Home from "./components/home";
+
+function App() {
+  return (
+  <div>
+    <div>
+     <BrowserRouter>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/login" element={<LoginPage/>} />
+            <Route path="/register" element={<SignupPage/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  </div>
+  );
+}
+
+export default App;
---
+++ front-end react/mini-project/src/components/Signup.js
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { signupFields } from "../constants/formFields"
+import FormAction from "./FormAction";
+import Input from "./Input";
+
+const fields=signupFields;
+let fieldsState={};
+
+fields.forEach(field => fieldsState[field.id]='');
+fieldsState.confirmPassword = '';
+
+export default function Signup(){
+  const [signupState,setSignupState]=useState(fieldsState);
+
+  const handleChange=(e)=>setSignupState({...signupState,[e.target.id]:e.target.value});
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+
+    if (signupState.password !== signupState.confirmpassword) {
+      console.log("Passwords do not match");
+      alert("passwords do not match")
+      return;
+    }
+    console.log(signupState)
+    createAccount()
+  }
+
+  //handle Signup API Integration here
+  const createAccount=()=>{
+
+  }
+
+    return(
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+        <div className="">
+        {
+                fields.map(field=>
+                        <Input
+                            key={field.id}
+                            handleChange={handleChange}
+                            value={signupState[field.id]}
+                            labelText={field.labelText}
+                            labelFor={field.labelFor}
+                            id={field.id}
+                            name={field.name}
+                            type={field.type}
+                            isRequired={field.isRequired}
+                            placeholder={field.placeholder}
+                    />
+                
+                )
+            }
+          <FormAction handleSubmit={handleSubmit} text="Signup" />
+        </div>
+
+         
+
+      </form>
+    )
+}
