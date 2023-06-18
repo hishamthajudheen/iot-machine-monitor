72e71969ab848d1e13f8258316a1a74c9830b0ce
Hisham Thajudheen <hishamthajudheen@gmail.com>
2023-06-18 14:03:12 +0530

    tweaked the register user page to include some more info

--- iotMonitorApi/members/views.py
+++ iotMonitorApi/members/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
+
 
 def login_user(request):
     if request.method =="POST":
@@ -27,7 +29,7 @@ def logout_user(request):
 
 def register_user(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterUserForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data['username']
@@ -37,5 +39,5 @@ def register_user(request):
             messages.success(request,"Registration Successful!")
             return redirect('login')
     else:
-        form = UserCreationForm()    
+        form = RegisterUserForm()    
     return render(request,'authenticate/register_user.html', {'form':form})
