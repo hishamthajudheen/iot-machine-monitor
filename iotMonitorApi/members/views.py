eb71dfbb849066faa4af4a82f44e71e1eae385c7
Hisham Thajudheen <hishamthajudheen@gmail.com>
2023-06-18 13:33:56 +0530

    Added user registration to the auth system

--- iotMonitorApi/members/views.py
+++ iotMonitorApi/members/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 def login_user(request):
     if request.method =="POST":
@@ -23,3 +24,18 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("You were logged out!"))
     return redirect('login')
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request,"Registration Successful!")
+            return redirect('login')
+    else:
+        form = UserCreationForm()    
+    return render(request,'authenticate/register_user.html', {'form':form})
