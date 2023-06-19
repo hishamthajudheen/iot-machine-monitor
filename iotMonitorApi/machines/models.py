5e69bf879326f087e434b1f25c31bd0491861bec
Hisham Thajudheen <hishamthajudheen@gmail.com>
2023-06-19 08:11:25 +0530

    Tweaks to user reg

--- iotMonitorApi/machines/models.py
+++ iotMonitorApi/machines/models.py
@@ -4,9 +4,9 @@ from django.core.validators import MinLengthValidator
 
 class Cpu(models.Model):
     cpu_name = models.CharField(max_length=30, default="cpu_name")
-    fan_speed = models.DecimalField(max_digits=6,decimal_places=3)
-    cpu_temp = models.DecimalField(max_digits=4, decimal_places=3)
-    gpu_temp = models.DecimalField(max_digits=4, decimal_places=3)
+    fan_speed = models.DecimalField(max_digits=6,decimal_places=3, blank=True)
+    cpu_temp = models.DecimalField(max_digits=6, decimal_places=3, blank=True)
+    gpu_temp = models.DecimalField(max_digits=6, decimal_places=3, blank=True)
     
     def __str__(self):
         return self.cpu_name
