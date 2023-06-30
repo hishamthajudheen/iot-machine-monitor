fd0e97a4725076247f97e975340732dfd1f4af51
Hisham Thajudheen <hishamthajudheen@gmail.com>
2023-07-01 02:45:54 +0530

    Did some bare-bones styling to login page

--- iotMonitorApi/members/forms.py
+++ iotMonitorApi/members/forms.py
@@ -5,7 +5,7 @@ from django import forms
 class RegisterUserForm(UserCreationForm):
     email = forms.EmailField(required=False)
     first_name = forms.CharField(max_length=50, required=True)
-    last_name = forms.CharField(max_length=50, required=True)
+    last_name = forms.CharField(max_length=50)
     
     class Meta:
         model = User
